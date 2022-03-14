916453e80e8218444d66cd8a8c239ab57d28ffdc
divyesh parmar <divyeshparmar112001@gmail.com>
2022-03-14 17:18:19 +0530

    s change

--- client/src/componets/Buyer/Buyer.js
+++ client/src/componets/Buyer/Buyer.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 
 
 export default function Buyer() {
+
+  const [crop, setcrop] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        "http://localhost:9002/uploadcrop/" 
+        
+      )
+      .then((res) => {
+        console.log(res);   
+        setcrop(res.data);
+        console.log(crop);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const cart = () => {
+      
+  }
+
+
   return (
     <>
       <section className="container buyer-container">
@@ -20,8 +44,43 @@ export default function Buyer() {
             </Link>
           </header>
         </div>
+        <table>
+        <thead className="far-product">
+          <tr className= "far">
+            <th>No</th>
+            <th>Farmername</th>
+            {/* <h2>Date</h2> */}
+            <th>City</th>
+            <th>Cropname</th>
+            <th>CropQuantity(kg)</th>
+            <th>CropDesc</th>
+            {/* <h2>CropImg</h2> */}
+            <th>CropPrice(Rs/kg)</th>
+            {/* <h2>Modify</h2> */}
+          </tr>
+        </thead>
+        <tbody className="far-product">
+          {crop.map((item, index) => (
+            <tr className="far" key={index}>
+              <td>{index + 1} </td>
+              <td>{item.username}</td>
+              {/* <h2>12 March</h2> */}
+              <td>{item.city}</td>
+              <td>{item.cropname}</td>
+              <td>{item.cropQuantity}</td>
+              <td>{item.cropDescription}</td>
+              {/* <td>{item.logo}</td> */}
+              <td>{item.cropprice}</td>
+              {/* <td>{item.Modify}</td> */}
+              <a href="#" class="btn cart px-auto" onClick={cart}>
+                  ADD TO CART
+                </a>
+            </tr>
+          ))}
+        </tbody>
+        </table>
         
-        <div className="row d-flex justify-content-center">
+        {/* <div className="row d-flex justify-content-center">
     
           <div class="card mt-5 mx-4">
             {" "}
@@ -80,8 +139,8 @@ export default function Buyer() {
                 </a>
               </div>
             </div>
-          </div>
-        </div>
+           </div> 
+        </div>*/}
         
       </section>
     </>
